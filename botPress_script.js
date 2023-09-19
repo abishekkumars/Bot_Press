56724f90745e364157823fa7ee8f1be462eb151d
Abishekkumar <97280327+abishekkumars@users.noreply.github.com>
2023-09-19 20:14:54 +0530

    Update botPress_script.js

--- botPress_script.js
+++ botPress_script.js
@@ -1,2 +1,10 @@
-<script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
-<script src="https://mediafiles.botpress.cloud/97d8b994-9b9b-496f-a36f-ab52e5128b20/webchat/config.js" defer></script>
+// Load the BotPress webchat
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://cdn.botpress.cloud/webchat/v0/inject.js';
+document.head.appendChild(scriptElement);
+
+// Load the webchat configuration
+var configScriptElement = document.createElement('script');
+configScriptElement.src = 'https://mediafiles.botpress.cloud/97d8b994-9b9b-496f-a36f-ab52e5128b20/webchat/config.js';
+configScriptElement.defer = true;
+document.head.appendChild(configScriptElement);
