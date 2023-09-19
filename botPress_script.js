1ed44fb7f50673b4cf84aa5896c55c70b43638b5
Abishekkumar <97280327+abishekkumars@users.noreply.github.com>
2023-09-19 19:51:43 +0530

    Init

---
+++ botPress_script.js
@@ -0,0 +1,2 @@
+<script src="https://cdn.botpress.cloud/webchat/v0/inject.js"></script>
+<script src="https://mediafiles.botpress.cloud/97d8b994-9b9b-496f-a36f-ab52e5128b20/webchat/config.js" defer></script>
